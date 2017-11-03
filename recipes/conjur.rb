f3d26dc0c3915bb8af508878a03aeb6a05ec5037
CaptainFluffyToes <DJKhan85@gmail.com>
2017-11-03 10:37:04 -0400

    added timers for genreating the API keys.

--- recipes/conjur.rb
+++ recipes/conjur.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2017, Darren Khan , All Rights Reserved.
 
+conjur_network = 'pipeline'
+conjur_name = 'conjur-master'
+account_name = 'ConUser'
+
 images = ['cyberark/conjur', 'postgres', 'conjurinc/cli5']
 
 images.each do |image|
@@ -13,54 +17,67 @@ images.each do |image|
   end
 end
 
-CONJUR_NETWORK = 'pipeline'
-CONJUR_NAME = 'conjur-master'
-ACCOUNT_NAME = 'ConUser'
+docker_network "#{conjur_network}" do
+  action :create
+end
 
 ruby_block 'Generate_data_Key' do
   block do
     node.default['conjur']['data_key'] = `docker container run --rm cyberark/conjur data-key generate`
   end
-  notifies :run, 'docker_container[conjur]', :delayed
+  notifies :run, 'docker_container[database]', :immediate
   action :run
-end
-
-docker_network "#{CONJUR_NETWORK}" do
-  action :create
 end
 
 docker_container 'database' do
   container_name 'database'
-  network_mode "#{CONJUR_NETWORK}"
+  network_mode "#{conjur_network}"
   repo 'postgres'
-  action :run
+  notifies :run, 'docker_container[conjur]', :immediate
+  action :nothing
 end
 
 docker_container 'conjur' do
-  container_name "#{CONJUR_NAME}"
-  network_mode "#{CONJUR_NETWORK}"
+  container_name "#{conjur_name}"
+  network_mode "#{conjur_network}"
   repo 'cyberark/conjur'
   command 'server'
   port '3000:3000'
   env lazy { ['DATABASE_URL=postgres://postgres@database/postgres', "CONJUR_DATA_KEY=#{node['conjur']['data_key']}"] }
+  notifies :run, 'ruby_block[sleep]', :immediate
+  action :nothing
+end
+
+ruby_block 'sleep' do
+  block do
+    sleep 10
+  end
   notifies :run, 'ruby_block[Generate_API_Key]', :immediate
   action :nothing
 end
 
 ruby_block 'Generate_API_Key' do
   block do
-    node.default['conjur']['account_api'] = `docker exec #{CONJUR_NAME} conjurctl account create #{ACCOUNT_NAME} | awk '/admin:/{print $5}'`
+    node.default['conjur']['account_api'] = `docker exec #{conjur_name} conjurctl account create #{account_name} | awk '/admin:/{print $5}'`
   end
-  notifies :run, 'docker_container[cli]', :delayed
+  notifies :run, 'ruby_block[sleep_key]', :immediate
+  action :nothing
+end
+
+ruby_block 'sleep_key' do
+  block do
+    sleep 10
+  end
+  notifies :run, 'docker_container[cli]', :immediate
   action :nothing
 end
 
 docker_container 'cli' do
   container_name 'conjur-cli'
-  network_mode "#{CONJUR_NETWORK}"
+  network_mode "#{conjur_network}"
   repo 'conjurinc/cli5'
   command 'infinity'
   entrypoint 'sleep'
-  env lazy { ["CONJUR_APPLIANCE_URL=http://#{CONJUR_NAME}", "CONJUR_ACCOUNT=#{ACCOUNT_NAME}", "CONJUR_AUTHN_API_KEY=#{node['conjur']['account_api']}", 'CONJUR_AUTHN_LOGIN=admin'] }
+  env lazy { ["CONJUR_APPLIANCE_URL=http://#{conjur_name}", "CONJUR_ACCOUNT=#{account_name}", "CONJUR_AUTHN_API_KEY=#{node['conjur']['account_api']}", 'CONJUR_AUTHN_LOGIN=admin'] }
   action :nothing
 end
