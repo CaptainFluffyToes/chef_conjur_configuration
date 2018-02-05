00b5d2e21d1eaf2a6e3cd319faf68f6a0c45fc02
CaptainFluffyToes <DJKhan85@gmail.com>
2018-02-05 16:56:48 -0500

    changed the name of the organization

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'djkhan85@gmail.com'
 license 'GPL-3.0'
 description 'Installs/Configures conjur_configuration'
 long_description 'Installs/Configures conjur_configuration'
-version '0.1.4'
+version '0.1.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -24,5 +24,6 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # [10/16/2017] - 0.1.2 - added images and containers for docker install.
 # [10/18/2017] - 0.1.3 - created loops for multi item resources.
 # [10/30/2017] - 0.1.4 - Added new command for generating the datakey
+# [02/05/2018] - 0.1.5 - Trimmed new line after the creation of the API key.  Updated organization name. 
 
 depends 'docker_configuration'
